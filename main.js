22c89c070d639d163b2013ac546392d6459f3e91
Agus27111 <agussetiawan.belajar@gmail.com>
2023-12-10 04:00:26 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// if we hit button plus pHide will  remove class hidden (display =block)
+// and button will change into minus
+// if we hit button minus pHide will add class hidden (display =none)
+
+const buttonPlus = document.querySelectorAll(".plus");
+const buttonMinus = document.querySelectorAll(".minus");
+const pHide = document.querySelectorAll(".pHide");
