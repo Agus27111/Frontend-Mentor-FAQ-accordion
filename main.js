a00407e8665fd20a96856ec88bdcc11195966e00
Agus27111 <agussetiawan.belajar@gmail.com>
2023-12-10 05:54:25 +0700

    add JS

--- main.js
+++ main.js
@@ -2,6 +2,35 @@
 // and button will change into minus
 // if we hit button minus pHide will add class hidden (display =none)
 
-const buttonPlus = document.querySelectorAll(".plus");
-const buttonMinus = document.querySelectorAll(".minus");
-const pHide = document.querySelectorAll(".pHide");
+const buttonHide = document.querySelectorAll(".button");
+
+buttonHide.forEach((button) => {
+  button.addEventListener("click", function () {
+    const parentItem = this.closest(".item");
+    const buttonPlus = parentItem.querySelector(".plus");
+    const buttonMinus = parentItem.querySelector(".minus");
+    const pHide = parentItem.querySelector(".pHide");
+
+    pHide.classList.toggle("hidden");
+
+    //to replace img icon plus and minus
+    buttonMinus.classList.toggle("hidden");
+    buttonPlus.classList.toggle("hidden");
+  });
+});
+
+//My first Try
+
+// const buttonHide = document.querySelectorAll(".button");
+
+// buttonHide.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const buttonPlus = document.querySelectorAll(".plus");
+//     const buttonMinus = document.querySelectorAll(".minus");
+//     const pHide = document.querySelectorAll(".pHide");
+
+//     pHide.classList.toggle("hidden");
+//     buttonMinus.classList.toggle("hidden");
+//     buttonPlus.classList.toggle("hidden");
+//   });
+// });
