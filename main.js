5524db892578af2cd98f164a61ef8604824d1315
Agus27111 <agussetiawan.belajar@gmail.com>
2023-12-12 03:49:10 +0700

    add JS to handel backround but didnt finished yet

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const buttonHide = document.querySelectorAll(".button");
 
 buttonHide.forEach((button) => {
   button.addEventListener("click", function () {
-    const parentItem = this.closest(".item");
+    const parentItem = this.closest(".item"); //untuk menyeleksi button mana yang aktif dan pHide yang harus aktif.
+    //closest() lebih baik dibandingkan dengan document.querySelector....
     const buttonPlus = parentItem.querySelector(".plus");
     const buttonMinus = parentItem.querySelector(".minus");
     const pHide = parentItem.querySelector(".pHide");
@@ -18,6 +19,28 @@ buttonHide.forEach((button) => {
     buttonPlus.classList.toggle("hidden");
   });
 });
+
+// for the backround
+
+// Get the reference to the image element
+let mobile = document.getElementById("bgImg");
+let desktop = document.getElementById("bgImg2");
+
+// Function to check the screen size and apply the background image accordingly
+function setBackgroundImage() {
+  if (window.matchMedia("(min-width: 1024px)").matches) {
+    // For desktop screens (min-width: 1024px)
+    mobile.document.classList.add("hidden");
+  } else {
+    desktop.document.classList.add("hidden");
+  }
+}
+
+// Call the function initially when the page loads
+setBackgroundImage();
+
+// Listen for window resize events and update the background image accordingly
+window.addEventListener("resize", setBackgroundImage);
 
 //My first Try
 
